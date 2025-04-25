8a5c5d6638cef73ffd8ef92aaaa979df2e1437ef
ShubhamDidharia <shubhamdidharia627@gmail.com>
2025-04-26 00:10:38 +0530

    post-deployment fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // Adjust if your app is served from a subpath
+  
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // Proxy API requests to the backend
+      '/api': 'http://localhost:5000',  // Proxy API requests to local backend in dev
     },
   },
 })
